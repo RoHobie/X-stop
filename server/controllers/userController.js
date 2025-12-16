18cae35893166e78a7a3aad93826770fe61bba2e
RoHobie <ronitthakur267@gmail.com>
2025-12-17 03:51:05 +0530

    add existing uses check and validation

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,9 +1,30 @@
+import validator from 'validator';
 const loginUser = async (req, res) => {
 
 };
 
 const registerUser = async (req, res) => {
-    res.json({ msg:"Register API working"})
+    
+    try{
+        const { username, email, password } = req.body;
+        //checking if user exists
+        const existingUser = await userModel.findOne({ email });
+        if(existingUser){
+            return res.status(400).json({ message: 'User already exists' });
+        }
+
+        //validation
+        if(!validator.isEmail(email)){
+            return res.status(400).json({ message: 'Invalid email format' });
+        }
+        if(!validator.isStrongPassword(password, {
+            minLength: 8,
+        })){
+            return res.status(400).json({ message: 'Password is not strong enough' });
+        }
+    } catch (error) {
+
+    }
 };
 
 const adminLogin = async (req, res) => {
