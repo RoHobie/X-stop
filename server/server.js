696942c3cf506e370b7a85964fa517f314e53245
RoHobie <ronitthakur267@gmail.com>
2025-12-17 02:55:46 +0530

    import and call connectCloudinary

--- server/server.js
+++ server/server.js
@@ -2,11 +2,13 @@ import express from 'express';
 import cors from 'cors';
 import 'dotenv/config';
 import connectDB from './config/mongodb.js';
+import connectCloudinary from './config/cloudinary.js';
 
 // App setup
 const app = express();
 const PORT = process.env.PORT || 5000;
 connectDB();
+connectCloudinary();
 
 // Middleware
 app.use(express.json());
